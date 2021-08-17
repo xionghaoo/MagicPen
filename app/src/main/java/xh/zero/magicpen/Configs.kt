9d5ecbdd8705b678bce0cc4137b5027c6fd0b62d
zero <xionghao930316@gmail.com>
2021-08-17 19:41:49 +0800

    设备信息

--- app/src/main/java/xh/zero/magicpen/Configs.kt
+++ app/src/main/java/xh/zero/magicpen/Configs.kt
@@ -3,5 +3,12 @@ package xh.zero.magicpen
 class Configs {
     companion object {
         const val DEVICE_MAC_ADDRESS = "A4:C1:38:0A:7F:10"
+
+        // 设备信息
+        const val SERVICE_DEVICE_INFO = "0000180A-0000-1000-8000-00805F9B34FB"
+        const val SERVICE_SN = "00002A25-0000-1000-8000-00805F9B34FB"
+        const val SERVICE_FIRMWARE = "00002A2B-0000-1000-8000-00805F9B34FB"
+
+
     }
 }
