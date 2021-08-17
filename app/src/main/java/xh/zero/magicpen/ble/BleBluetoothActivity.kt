a829299f0a698d5c57cf2c12a4749e5b476cb3b5
zero <xionghao930316@gmail.com>
2021-08-17 16:58:05 +0800

    服务发现

--- app/src/main/java/xh/zero/magicpen/ble/BleBluetoothActivity.kt
+++ app/src/main/java/xh/zero/magicpen/ble/BleBluetoothActivity.kt
@@ -1,28 +1,36 @@
 package xh.zero.magicpen.ble
 
 import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
+import android.app.Service
+import android.bluetooth.*
+import android.bluetooth.BluetoothDevice.TRANSPORT_LE
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import pub.devrel.easypermissions.AfterPermissionGranted
 import pub.devrel.easypermissions.EasyPermissions
-import xh.zero.magicpen.BuildConfig
 import xh.zero.magicpen.Configs
+import xh.zero.magicpen.R
 import xh.zero.magicpen.ToastUtil
 import xh.zero.magicpen.databinding.ActivityBleBluetoothBinding
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class BleBluetoothActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_ENABLE_BT = 1
         private const val RC_PERMISSION_LOCATION = 2
-        private const val SCAN_PERIOD = 20_000L
+        private const val SCAN_PERIOD = 120_000L
+
+        private const val TAG = "BleBluetoothActivity"
     }
 
     private lateinit var binding: ActivityBleBluetoothBinding
@@ -40,15 +48,73 @@ class BleBluetoothActivity : AppCompatActivity() {
     private var mScanning: Boolean = false
 
     private val handler = Handler()
-
     private lateinit var bleDeviceAdapter: BleDeviceAdapter
+
+    var bluetoothGatt: BluetoothGatt? = null
 
     private val leScanCallback = BluetoothAdapter.LeScanCallback { device, rssi, scanRecord ->
         runOnUiThread {
-            val item = BleDevice()
-            item.name = device.name
-            item.macAddr = if (device.address == Configs.DEVICE_MAC_ADDRESS) device.address + "- right" else device.address
-            bleDeviceAdapter.addDevice(item)
+            // 扫描结果
+            if (device.address == Configs.DEVICE_MAC_ADDRESS) {
+                bleDeviceAdapter.addDevice(device)
+            }
+        }
+    }
+
+//    private var connectionState = STATE_DISCONNECTED
+
+    // Various callback methods defined by the BLE API.
+    private val gattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(
+            gatt: BluetoothGatt,
+            status: Int,
+            newState: Int
+        ) {
+            val intentAction: String
+            when (newState) {
+                BluetoothProfile.STATE_CONNECTED -> {
+                    intentAction = ACTION_GATT_CONNECTED
+//                    connectionState = STATE_CONNECTED
+                    broadcastUpdate(intentAction)
+                    Log.i(TAG, "Connected to GATT server.")
+                    Log.i(TAG, "Attempting to start service discovery: " + bluetoothGatt?.discoverServices())
+                }
+                BluetoothProfile.STATE_DISCONNECTED -> {
+                    intentAction = ACTION_GATT_DISCONNECTED
+//                    connectionState = STATE_DISCONNECTED
+                    Log.i(TAG, "Disconnected from GATT server.")
+                    broadcastUpdate(intentAction)
+                }
+            }
+        }
+
+        // New services discovered
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+
+            Log.d(TAG, "onServicesDiscovered: ${gatt.services.size}, ${status}")
+            gatt.services.forEach { service ->
+                Log.d(TAG, "uuid: ${service.uuid}, characteristic: ${service.characteristics.size}, ${service.characteristics}")
+            }
+            when (status) {
+                BluetoothGatt.GATT_SUCCESS -> broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED)
+                else -> Log.w(TAG, "onServicesDiscovered received: $status")
+            }
+
+//            gatt.writeCharacteristic(BluetoothGattCharacteristic(, , BluetoothGattCharacteristic.PROPERTY_NOTIFY))
+        }
+
+        // Result of a characteristic read operation
+        override fun onCharacteristicRead(
+            gatt: BluetoothGatt,
+            characteristic: BluetoothGattCharacteristic,
+            status: Int
+        ) {
+            Log.d(TAG, "onCharacteristicRead: characteristic, ${characteristic.uuid}")
+            when (status) {
+                BluetoothGatt.GATT_SUCCESS -> {
+                    broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic)
+                }
+            }
         }
     }
 
@@ -71,11 +137,20 @@ class BleBluetoothActivity : AppCompatActivity() {
         }
 
         binding.rcDevices.layoutManager = LinearLayoutManager(this)
-        bleDeviceAdapter = BleDeviceAdapter(ArrayList())
+        bleDeviceAdapter = BleDeviceAdapter(ArrayList()) { device ->
+            Log.d(TAG, "开始连接设备")
+            bluetoothGatt = device.connectGatt(this, true, gattCallback, TRANSPORT_LE)
+            scanLeDevice(false)
+        }
         binding.rcDevices.adapter = bleDeviceAdapter
+
 
         binding.btnScan.setOnClickListener {
             scanTask()
+        }
+
+        binding.btnClose.setOnClickListener {
+            close()
         }
     }
 
@@ -123,4 +198,56 @@ class BleBluetoothActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun broadcastUpdate(action: String) {
+        val intent = Intent(action)
+        sendBroadcast(intent)
+    }
+
+    private fun broadcastUpdate(action: String, characteristic: BluetoothGattCharacteristic) {
+        val intent = Intent(action)
+        Log.d(TAG, "broadcastUpdate: uuid = ${characteristic.uuid}")
+        // This is special handling for the Heart Rate Measurement profile. Data
+        // parsing is carried out as per profile specifications.
+        when (characteristic.uuid) {
+            UUID.fromString("0000EEE0-0000-1000-8000-00805F9B34FB") -> {
+                // 五、	APP跟遥控器的命令交互
+                val flag = characteristic.properties
+                Log.d(TAG, "控制命令： ${flag}")
+//                val format = when (flag and 0x01) {
+//                    0x01 -> {
+//                        Log.d(TAG, "Heart rate format UINT16.")
+//                        BluetoothGattCharacteristic.FORMAT_UINT16
+//                    }
+//                    else -> {
+//                        Log.d(TAG, "Heart rate format UINT8.")
+//                        BluetoothGattCharacteristic.FORMAT_UINT8
+//                    }
+//                }
+//                val heartRate = characteristic.getIntValue(format, 1)
+//                Log.d(TAG, String.format("Received heart rate: %d", heartRate))
+//                intent.putExtra(EXTRA_DATA, (heartRate).toString())
+            }
+            else -> {
+                // For all other profiles, writes the data formatted in HEX.
+                Log.d(TAG, "其他特征值： ${characteristic}")
+
+//                val data: ByteArray? = characteristic.value
+//                if (data?.isNotEmpty() == true) {
+//                    val hexString: String = data.joinToString(separator = " ") {
+//                        String.format("%02X", it)
+//                    }
+//                    intent.putExtra(EXTRA_DATA, "$data\n$hexString")
+//                }
+            }
+
+        }
+        sendBroadcast(intent)
+    }
+
+    fun close() {
+        bluetoothGatt?.close()
+        bluetoothGatt = null
+    }
+
 }
--- app/src/main/java/xh/zero/magicpen/ble/BleDeviceAdapter.kt
+++ app/src/main/java/xh/zero/magicpen/ble/BleDeviceAdapter.kt
@@ -1,19 +1,32 @@
 package xh.zero.magicpen.ble
 
+import android.bluetooth.BluetoothDevice
 import android.view.View
 import android.widget.TextView
+import xh.zero.magicpen.Configs
 import xh.zero.magicpen.PlainListAdapter
 import xh.zero.magicpen.R
 
-class BleDeviceAdapter(val items: ArrayList<BleDevice>) : PlainListAdapter<BleDevice>(items) {
+class BleDeviceAdapter(
+    val items: ArrayList<BluetoothDevice>,
+    private val onItemClick: (device: BluetoothDevice) -> Unit
+) : PlainListAdapter<BluetoothDevice>(items) {
     override fun itemLayoutId(): Int = R.layout.list_item_ble_device
 
-    override fun bindView(v: View, item: BleDevice, position: Int) {
+    override fun bindView(v: View, item: BluetoothDevice, position: Int) {
         val tv = v as TextView
-        tv.text = "${item.name} - ${item.macAddr}"
+        if (item.address == Configs.DEVICE_MAC_ADDRESS) {
+            tv.text = "${item.name} - ${item.address} - found"
+        } else {
+            tv.text = "${item.name} - ${item.address}"
+        }
+
+        tv.setOnClickListener {
+            onItemClick(item)
+        }
     }
 
-    fun addDevice(device: BleDevice) {
+    fun addDevice(device: BluetoothDevice) {
         items.add(device)
         notifyDataSetChanged()
     }
